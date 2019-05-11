c86d0e096d3f66e60eac552a1e5ba44b804b77f0
Berwout de Vries Robles <berwoutv@infosupport.com>
2019-05-11 20:12:26 +0200

    start of the project

---
+++ src/main/java/com/infosupport/examplepipeline/ExamplePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.infosupport.examplepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplePipelineApplication.class, args);
+	}
+
+}
