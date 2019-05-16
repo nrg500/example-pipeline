59b4357ce1caccdaea4f676297be53c1a9886d34
Berwout de Vries Robles <berwoutv@infosupport.com>
2019-05-16 09:46:55 +0200

    variabele naam toegevoegd

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM adoptopenjdk/openjdk11
-COPY target/example-pipeline-0.0.1-SNAPSHOT.jar /app.jar
-CMD ["java", "-jar", "-Dspring.profiles.active=default", "/app.jar"]
+ADD target/example-pipeline-0.0.1-SNAPSHOT.jar /app.jar
+CMD ["java", "-jar", "/app.jar"]
--- src/main/java/com/infosupport/examplepipeline/HelloWorldController.java
+++ src/main/java/com/infosupport/examplepipeline/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.infosupport.examplepipeline;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping(value="/hello")
-    public String sayHello(){
-        return "Hello from the HelloWorldController";
+    @GetMapping(value="/hello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "Hello from " + name;
     }
 }
